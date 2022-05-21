03094b6240a0fd2c671330d858d5b3810bc0140b
renepiik <renepiik@gmail.com>
2022-05-21 11:06:56 +0300

    added guard against tweeting after year ends

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 This is a twitter bot which tracks the progress of the ongoing semester at the University of Tartu.
 Inspired by the account @year_progress.
 
+Uses geckodriver for web scraping, the binary must be placed in PATH for it to work.
+
 Link to this bot's account: [@unitartu_prgress](https://twitter.com/unitartu_prgrss)
--- twitter_bot.py
+++ twitter_bot.py
@@ -1,4 +1,4 @@
-# import tweepy
+import tweepy
 import datetime
 from dotenv import dotenv_values
 import date_scraper
@@ -6,20 +6,21 @@ import date_scraper
 # config
 config = dotenv_values(".env")
 
-'''
 auth = tweepy.OAuthHandler(config['CONSUMER_KEY'], config['CONSUMER_SECRET'])
 auth.set_access_token(config['ACCESS_KEY'], config['ACCESS_SECRET'])
 api = tweepy.API(auth)
-'''
+
 
 # ongoing semester start and end times
 # UT does not provide any endpoint to get those dates easily so i have to resort to web crawling a page which might change at any time and fuck up this script, THANKS!
-
 semester_start, semester_end = date_scraper.main()
 today = datetime.date.today()
 
 # progress x/100
 semester_progress = round(((today - semester_start) * 100) / (semester_end - semester_start))
+
+if semester_progress > 100:
+    raise Exception("Semester is over!")
 
 # convert it into y/15
 mapped_progress = round((semester_progress / 100) * 15)
@@ -34,7 +35,6 @@ for i in range(15):
 status = progress_bar + '  ' + str(semester_progress) + '%'
 
 try:
-    print(status)
     api.update_status(status)
     print('New Tweet posted!')
 except Exception:
